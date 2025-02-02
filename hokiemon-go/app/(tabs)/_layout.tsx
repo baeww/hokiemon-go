f30190ad432e64773575b0ec3528b0f6a235507f
William Bae <wbae@vt.edu>
2025-02-01 22:37:35 -0500

    friends tab init

--- hokiemon-go/app/(tabs)/_layout.tsx
+++ hokiemon-go/app/(tabs)/_layout.tsx
@@ -41,9 +41,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="friends"
         options={{
-          title: 'Explore',
+          title: 'Friends',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
